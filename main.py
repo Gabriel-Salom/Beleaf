7a0f1cc9052443952da56cd53cf5af069c246936
unknown <gabrielsalom@hotmail.com>
2020-10-08 15:33:07 -0300

    Chart and config funcionando

--- main.py
+++ main.py
@@ -55,62 +55,6 @@ class Config(db.Model):
         self.time_off = time_off
 
 
-# Measurement Schema
-class MeasurementSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'date_posted', 'humidity', 'light', 'temperature', 'ph', 'conductivity')
-
-class Config_Schema(ma.Schema):
-    class Meta:
-        fields = ('id', 'lux_max', 'lux_min', 'time_on' , 'time_off')
-
-
-# Init Schema
-measurement_schema = MeasurementSchema()
-measurements_schema = MeasurementSchema(many=True)
-
-# Recieving data from esp32
-@app.route('/value', methods=['POST'])
-def add_measurement():
-    now = datetime.now()
-    date_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    request.json['date_posted'] = date_string
-    date_posted = request.json['date_posted']
-    humidity = request.json['humidity']
-    light = request.json['light']
-    temperature = request.json['temperature']
-    ph = request.json['ph']
-    conductivity = request.json['conductivity']
-    new_measurement = Measurement(date_posted, humidity, light, temperature, ph, conductivity)
-    print(new_measurement)
-    db.session.add(new_measurement)
-    db.session.commit()
-    
-    return measurement_schema.jsonify(new_measurement)
-
-# Recieving data from front end
-@app.route('/config_elements', methods=['POST'])
-def config_elements():
-    config_entity = Config.query.filter_by(id=0).first()
-    print(config_entity)
-    if config_entity == None:
-        lux_max = 60
-        lux_min = 40
-        time_on = 20
-        time_off = 20
-        new_config = Config(lux_max, lux_min, time_on, time_off)
-        db.session.add(new_config)
-        db.session.commit()
-    else:
-        config_entity.lux_max = request.json['lux_max']
-        config_entity.lux_min = request.json['lux_min']
-        config_entity.time_on = request.json['time_on']
-        config_entity.time_off = request.json['time_off']
-        # new_config = Config(lux_max, lux_min, time_on , time_off)
-        db.session.commit()
-    print(config_entity)
-    return {}
-
 # Loading web page for users
 @app.route("/")
 def visualize_data():
@@ -118,7 +62,7 @@ def visualize_data():
     return render_template('graph.html', title='data', values = values)
 
 # Rest API for sending JSON to the front end
-resource_fields = {
+measurement_fields = {
     'id': fields.Integer,
     'date_posted': fields.String,
     'humidity': fields.Float,
@@ -128,13 +72,70 @@ resource_fields = {
     'conductivity': fields.Float
 }
 
+config_fields = {
+    'id': fields.Integer,
+    'lux_max': fields.Integer,
+    'lux_min': fields.Integer,
+    'time_on': fields.Integer,
+    'time_off': fields.Integer
+}
+
+# Restful API for sending and recieving measurements
 class Chart_data(Resource):
-    @marshal_with(resource_fields)
+    @marshal_with(measurement_fields)
     def get(self):
         result = Measurement.query.all()
         return result
+    def post(self):
+        now = datetime.now()
+        date_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        request.json['date_posted'] = date_string
+        date_posted = request.json['date_posted']
+        humidity = request.json['humidity']
+        light = request.json['light']
+        temperature = request.json['temperature']
+        ph = request.json['ph']
+        conductivity = request.json['conductivity']
+        new_measurement = Measurement(date_posted, humidity, light, temperature, ph, conductivity)
+        db.session.add(new_measurement)
+        db.session.commit()
+        return 201
+
+# Restful API for sending and recieving configurations
+class Config_data(Resource):
+    @marshal_with(config_fields)
+    def get(self):
+        result = Config.query.filter_by(id=1).first()
+        if result == None:
+            lux_max = 60
+            lux_min = 50
+            time_on = 20
+            time_off = 20
+            new_config = Config(lux_max, lux_min, time_on, time_off)
+            db.session.add(new_config)
+            db.session.commit()
+        result = Config.query.filter_by(id=1).first()
+        return result
+    def post(self):
+        result = Config.query.filter_by(id=1).first()
+        if result == None:
+            lux_max = 60
+            lux_min = 50
+            time_on = 20
+            time_off = 20
+            new_config = Config(lux_max, lux_min, time_on, time_off)
+            db.session.add(new_config)
+            db.session.commit()
+        else:
+            result.lux_max = request.json['lux_max']
+            result.lux_min = request.json['lux_min']
+            result.time_on = request.json['time_on']
+            result.time_off = request.json['time_off']
+            db.session.commit()
+        return 201
 
 api.add_resource(Chart_data, '/chart_data')
+api.add_resource(Config_data, '/config_elements')
 
 
 # Run Server
